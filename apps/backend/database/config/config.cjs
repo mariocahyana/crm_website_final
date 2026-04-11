2462a7f28da0af3218b4ecf2da7bbdce065db790
bagger101 <mariocahyana55@gmail.com>
2026-04-11 17:45:26 +0700

    env

---
+++ apps/backend/database/config/config.cjs
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || '0810',
+    database: process.env.DB_NAME || 'tubes_rpll',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'database_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'database_production',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: 'postgres',
+  },
+};
